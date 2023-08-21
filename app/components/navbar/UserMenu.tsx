73c63f6d1f4c4e9a7d84c7b52a2fc2d08c17ed4a
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-08-21 14:15:10 +0300

    NavBar Done

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,10 @@
 'use client';
+
+
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -55,8 +58,31 @@ const UserMenu = () => {
             </div>
             {isOpen && (
                 <div
-                className=''>
+                className='
+                absolute
+                rounded-xl
+                shadow-md
+                w-[40vw]
+                md:w-3/4
+                bg-white
+                overflow-hidden
+                right-0
+                top-12
+                text-sm
+                '>
+                    <div className='flex flex-col cursor-pointer'>
+                        <>
+                        <MenuItem
+                        onClick={() => {}}
+                        label="Login"
+                        />
+                        <MenuItem
+                        onClick={() => {}}
+                        label="Sign Up"
+                        />
+                        </>
 
+                    </div>
                 </div>
             )}
         </div>
