b5aed47fc889b4a85e6b68a3f4c9697f3dc78dda
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-09-07 15:33:20 +0300

    Country select

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,6 +2,7 @@
 
 import useRentModal from "@/app/hooks/useRentModal"
 import Modal from "./Modal"
+import CountrySelect from "../inputs/CountrySelect";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
@@ -110,12 +111,24 @@ const RentModal = () => {
             </div>
         </div>
     )
-
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                
+                />
+            </div>
+        )
+    }
     return (
         <Modal
         isOpen= {rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
